ee124bd8c5d32701dd1a54a7779c826336651dd1
Bhupesh <78196557+Bhupesh1x@users.noreply.github.com>
2024-01-05 19:45:09 +0530

    Updated the get listings server action to support all the filters.

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -2,16 +2,77 @@ import db from "../lib/db";
 
 export type IListingParams = {
   userId?: string;
+  roomCount?: number;
+  guestCount?: number;
+  bathroomCount?: number;
+  locationValue?: string;
+  startDate?: string;
+  endDate?: string;
+  category?: string;
 };
 
 export async function getListings(params: IListingParams) {
-  const { userId } = params;
+  const {
+    userId,
+    roomCount,
+    guestCount,
+    bathroomCount,
+    locationValue,
+    startDate,
+    endDate,
+    category,
+  } = params;
 
   try {
     let query: any = {};
 
     if (userId) {
       query.userId = userId;
+    }
+
+    if (category) {
+      query.category = category;
+    }
+
+    if (roomCount) {
+      query.roomCount = {
+        gte: +roomCount,
+      };
+    }
+
+    if (guestCount) {
+      query.guestCount = {
+        gte: +guestCount,
+      };
+    }
+
+    if (bathroomCount) {
+      query.bathroomCount = {
+        gte: +bathroomCount,
+      };
+    }
+
+    if (locationValue) {
+      query.locationValue = locationValue;
+    }
+
+    if (startDate && endDate) {
+      query.NOT = {
+        reservations: {
+          some: {
+            OR: [
+              {
+                startDate: { lte: startDate },
+                endDate: { gte: startDate },
+              },
+              {
+                startDate: { lte: endDate },
+                endDate: { gte: endDate },
+              },
+            ],
+          },
+        },
+      };
     }
 
     const listings = await db.listing.findMany({
